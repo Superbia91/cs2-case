7a982036997f325f26b480695ee42fa7a9bfca5a
Superbia Lol <shatarskii2005@gmail.com>
2026-01-28 19:10:01 +0300

    Initial commit

---
+++ src/main/java/net/superbia/caseopener/items/custom/CaseItem.java
@@ -0,0 +1,45 @@
+package net.superbia.caseopener.items.custom;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+
+public class CaseItem extends Item {
+
+
+    public CaseItem(Properties properties) {
+        super(properties);
+
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player user, InteractionHand hand) {
+        ItemStack stack = user.getItemInHand(hand);
+
+
+        if(world.isClientSide()){
+            return InteractionResultHolder.pass(stack);
+            //проверка на сервер/клиент
+        }
+
+        if(user.getAbilities().instabuild){
+            // проверка на креатив
+            return InteractionResultHolder.success(stack);
+        }else {
+            stack.shrink(1);
+            user.displayClientMessage(Component.literal("ВЫ ОТКРЫЛИ КЕЙС"),true);
+        }
+
+        return InteractionResultHolder.success(stack);
+
+
+
+    }
+
+}
